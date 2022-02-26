a95beb5494574b6d5f9d5b7e088c34dcfdf2a98b
andonedelia0 <98983798+andonedelia0@users.noreply.github.com>
2022-02-26 13:56:25 +0200

    Add files via upload

--- backend/endpoint.groovy
+++ backend/endpoint.groovy
@@ -1,19 +1,31 @@
+package backend
+import backend.methods
+
 @Grapes([
   @Grab('io.ratpack:ratpack-groovy:1.9.0'),
   @Grab('org.slf4j:slf4j-simple:1.7.30')
-  //grab mysql conn
 ])
 
 import groovy.sql.Sql
 import static ratpack.groovy.Groovy.ratpack
-//def map
 
+def Method = new methods()
+//def map
+Method.addun(5,6)
+Method.display()
+
+
+
+
+
+
+
+
+
+/*
 
 ratpack{
-    serverConfig {
-        port(5056)
-    }
-
+    
     handlers{
         get("tabla"){
            render "hello dawn"
@@ -22,15 +34,14 @@ ratpack{
         //se creeaza p. si se pun in memorie in structura mea de date care o sa fie un map(apoi fac tranzitia catre bd)
         //convertesc map ul in json =>se obt json-ul care se returneaza ca raspuns
         }
-
+        
         get("tabla/patratica/piese/:id"){
             render "astea sunt piesele"
         }
        
     }
 }
-
-
+*/
 
 
 
