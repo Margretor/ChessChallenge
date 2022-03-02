88704bc6f05d54f6c4f3ae71fd6f574d30c584e8
andonedelia0 <dandone@siqsess.ro>
2022-03-02 12:44:52 +0200

    vectorii pt nebun

--- backend/vect.groovy
+++ backend/vect.groovy
@@ -1,66 +1,67 @@
-def ci = 3
-def cj = 3
+def ci = 4
+def cj = 5
 def nr = 0
-//def pos = 8*ci +cj
+def j = cj
 def v1 = []  //stanga sus
-def v2 = []  //dreapta sus
-def v3 = []  //dreapta jos
+def v2 = []  //dreapta jos
+def v3 = []  //dreapta sus
 def v4 = []  //stanga jos
 //v[0]  = pos
 
-for(int i = ci; i>=0; i--){
-    for(int j = cj; j>=0; j--){
-        if(i == j) {        //diag principala
-            v1[nr]=8*i+j
-        }      
+for(int i = ci; i>=0; i--){ 
+    if((j>=0) && (i>=0)) {        
+        v1[nr]= 8 * i + j
+        j--
+        nr++
     }
-    nr++
 }
 nr = 0
-
-for(int i = ci; i>=0; i--){
-    for(int j = cj; j<=8-ci+1; j++){
-        if(j == ci+1 - i +1) {        //diag secundara
-            v2[nr]=8*i+j
-        }      
+j = cj
+for(int i = ci; i<8; i++){
+    if((j<=7) && (i<=7)) {        
+        v2[nr]= 8 * i + j
+        j++
+        nr++
     }
-    nr++
 }
 nr = 0
+j = cj
 
-for(int i = ci; i<=7; i++){
-    for(int j = cj; j<=7; j++){
-        if(i == j) {                //diag principala
-            v3[nr]=8*i+j
-        }      
+for(int i = ci; i>=0; i--){ 
+    if((j<=7) && (i>=0)) {        
+        v3[nr]= 8 * i + j
+        j++ 
+        nr++
     }
-    nr++
 }
 nr = 0
+j = cj
 
-for(int i = ci; i>=0; i--){
-    for(int j = cj; j>=0; j--){
-        if(i == j) {
-            v4[nr]=8*i+j
-        }      
+for(int i = ci; i<8; i++){
+    if((j>=0) && (i<=7)) {        
+        v4[nr]= 8 * i + j
+        j--
+        nr++
     }
-    nr++
 }
 
 
 println ("v1 este:")
-for(int k = 0; k<=ci; k++){
+for(int k = 0; k<v1.size; k++){
 println(v1[k])
 }
 println ("v2 este:")
-for(k = 0; k<=ci; k++){
+for(k = 0; k<v2.size; k++){
 println(v2[k])
 }
 println ("v3 este:")
-for(k = 0; k<8-ci; k++){      //de unde 5
-println(v3[k])
+for(k = 0; k<v3.size; k++){      
+    println(v3[k])
 }
+
 println ("v4 este:")
-for(k = 0; k<=ci; k++){
-println(v4[k])
+for(k = 0; k<v4.size; k++){
+    println(v4[k])
 }
+
+
